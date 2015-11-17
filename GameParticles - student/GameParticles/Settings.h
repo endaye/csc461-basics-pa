44e84299d810f703adc13e5a4851fddebff3bdee
Yuancheng Zhang <old.far.hot@gmail.com>
2015-11-17 15:44:15 -0600

    reduce the particle numbers

--- GameParticles - student/GameParticles/Settings.h
+++ GameParticles - student/GameParticles/Settings.h
@@ -4,7 +4,7 @@
 // here are 2 coarse knobs that you can adjust while refactoring this awesome code
 // Make sure you leave these setting to the default on final submission
 
-#define NUM_PARTICLES	(30 * 1000 ) //leave at 30K for final testing
+#define NUM_PARTICLES	(1 * 1000 ) //leave at 30K for final testing
 #define MAX_LIFE		(10.0f)  //leave at 10 for final testing
 
 
